8e4cc9e63e062a64a3de53e6b6abd2fbd920424c
Shivani-01 <60085524+Shivani-01@users.noreply.github.com>
2020-01-27 11:13:55 +0530

    Delete fn.py

--- Python-Asignment/Module 10/fn.py
+++
@@ -1,7 +0,0 @@
-import tkinter
-window=tkinter.Tk()
-window.title("GUI")
-def Tuto():
-    tkinter.Label(window,text="Hello I'm function").pack()
-tkinter.Button(window,text="Click me",command=Tuto).pack()
-window.mainloop()
